3a6605c9a7cf6037ad11177df2ef0d9cd8b12cd9
Sergio Valdez <sergioavr93@gmail.com>
2026-01-02 14:26:45 -0600

    Palabras reservadas a mayus

--- src/extension.ts
+++ src/extension.ts
@@ -13,10 +13,34 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('sql-fmtr.helloWorld', () => {
+	/* const disposable = vscode.commands.registerCommand('sql-fmtr.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from sql-fmtr! with TS');
+	}); */
+
+	const disposable = vscode.commands.registerCommand('sql-fmtr.helloWorld', function () {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) return;
+
+		const selection = editor.selection;
+		const text = editor.document.getText(selection);
+
+		const sqlKeywords = [
+			'SELECT', 'FROM', 'WHERE', 'INSERT', 'UPDATE', 'DELETE', 'CREATE', 'DROP',
+			'ALTER', 'TABLE', 'INDEX', 'VIEW', 'JOIN', 'INNER', 'LEFT', 'RIGHT', 'OUTER',
+			'ON', 'AS', 'AND', 'OR', 'NOT', 'IN', 'BETWEEN', 'LIKE', 'IS', 'NULL',
+			'ORDER', 'BY', 'GROUP', 'HAVING', 'DISTINCT', 'COUNT', 'SUM', 'AVG', 'MAX', 'MIN',
+			'CASE', 'WHEN', 'THEN', 'ELSE', 'END', 'INTO', 'VALUES', 'SET', 'UNION', 'ALL'
+		];
+
+		 const formatted = text.replace(/\b\w+\b/g, word => {
+			return sqlKeywords.includes(word.toUpperCase()) ? word.toUpperCase() : word;
+		});
+
+		editor.edit(editBuilder => {
+			editBuilder.replace(selection, formatted);
+		});
 	});
 
 	context.subscriptions.push(disposable);
